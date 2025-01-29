409cd2da35d2c2f18462698d44a22351e1285155
Iteratum <emmaogbo2002@gmail.com>
2025-01-29 23:28:44 +0100

    update the gitignore file

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ async def get_info():
     info = {
         "email": "emmaogbo2002@gmail.com",
         "current_datetime": datetime.now().isoformat(),
-        "github_url": "",
+        "github_url": "https://github.com/Iteratum/Stage0_hng_backend_track",
     }
     return info
 
